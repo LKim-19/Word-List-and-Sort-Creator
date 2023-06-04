60460aa0d497c7396f4dc605a49c19ef803a8050
Louis <47342278+LKim-19@users.noreply.github.com>
2023-06-04 12:47:55 -0700

    Add files via upload

---
+++ Word_List_and_Sort_Creator.py
@@ -0,0 +1,19 @@
+fname = "romeo.txt"
+
+fh = open(fname)
+lst = list()
+
+# For loop to split the lines up into individual words
+# In the file
+for line in fh:
+    x = line.split()
+    # For loop to loop through the words list
+    for y in x:
+        # If the word is not in the list it will
+        # Append to the list. If already exists doesn't append.
+        if y not in lst:
+            lst.append(y)
+
+lst.sort()ß
+
+print(lst)
